77540b9cb2921d9ed8ecd271c91cd51ea38caed8
Francy <f-digiuseppe.1@tudelft.nl>
2022-11-13 15:26:19 +0100

    aabbbb

--- Ktrash.py
+++ Ktrash.py
@@ -4,15 +4,9 @@ import pandas as pd
 from tabulate import tabulate
 
 start=time.time()
-np.random.seed(8)
 
-pd.set_option('display.max_columns', 500)
-df = pd.read_csv('EastWestAirlinesCluster.csv')
 
-array = df.to_numpy(dtype=np.float32)
-array2=array.copy()
-
-def normalize_min_max(array):
+def normalize_min_max(array):  #Normalizing the dataset. All entry will be contained between 0 and 1
     for i in range(1, array.shape[1]):
         array[:, i] = (array[:, i] - np.min(array[:, i])) / (np.max(array[:, i]) - np.min(array[:, i]))
 
@@ -23,12 +17,6 @@ def normalize_z_score(array):
         array[:, i] = (array[:, i] - np.mean(array[:, i])) / np.std(array[:, i])
 
     return array
-
-
-array = normalize_z_score(array)
-# array = normalize_min_max(array)
-array = np.delete(array, 0, 1)
-array2= np.delete(array2, 0, 1)
 
 def cluster_start(number_of_clusters, X): #Initialize the clusters randomly
     cluster_list = []
@@ -41,23 +29,30 @@ def cluster_start(number_of_clusters, X): #Initialize the clusters randomly
         cluster_list[assigned_list].append(i)   #Lists of elements in each cluster
     return cluster_list
 
-def kernel_matrix( X,g ):
-    #Hyperparameter to choose
+def G_kernel_matrix( X,g ):
     X_norm = np.sum(X ** 2, axis = -1)
-    K = np.exp(-g * (X_norm[:, None] + X_norm[None, :] - 2 * np.dot(X, X.T))) #This forumla helped so much, Time was aroun 90 seconds earlier now half a second. Not super precise though(maybe we need to play with the data types)
-    # for i in range(X.shape[0]): #doing this so thta we get 1 on the diagonals instead of 0.99998
-    #     K[i,i]=1
+    K = np.exp(-g * (X_norm[:, None] + X_norm[None, :] - 2 * np.dot(X, X.T)))
+    print(K)
     return K
 
-def un_normalize(X,back_info):
 
-    for i in range(X.shape[1]):
-            X[:, i] = (X[:, i] * back_info[i][0]) + back_info[i][1]
-    return
+def C_kernel_matrix(X, g): #Cauchy Kernel
+    X_norm = np.sum(X ** 2, axis=-1)
+    A = np.ones([X.shape[0],X.shape[0]])+g*(X_norm[:, None] + X_norm[None, :] - 2 * np.dot(X, X.T))
+    K=np.reciprocal(A)
+
+    return K
 
 
-def meanvar(clusterlist,kernelmatrix,clustersum):
-    SSE=0
+def E_kernel_matrix(X, g): #Cauchy Kernel
+    X_norm = np.sum(X ** 2, axis=-1)
+    A = np.ones([X.shape[0],X.shape[0]])+g*(X_norm[:, None] + X_norm[None, :] - 2 * np.dot(X, X.T))
+    K=np.reciprocal(A)
+
+    return K
+
+def ESD_calculator(clusterlist,kernelmatrix,clustersum):
+    ESD=0
 
     for k in range(NumberOfClusters):
         card = len(clusterlist[k])
@@ -66,9 +61,9 @@ def meanvar(clusterlist,kernelmatrix,clustersum):
             a = kernelmatrix[f, f]
             b = kernelmatrix[f, clusterlist[k]].sum()
             dfk= a-2/card * b + c/(card**2)
-            SSE=SSE + dfk
+            ESD=ESD + dfk
 
-    return SSE
+    return ESD
 def gamma_parameter(array):
     av = []
     for i in range(array.shape[1]):
@@ -87,12 +82,14 @@ def kernel_cluster(ker, ClusterList, NumberOfClusters):
     for k in range(NumberOfClusters):  # This is the sum of each cluster, or third term in the formula
         ClusterSum[k] = (ker[:, ClusterList[k]])[ClusterList[k], :].sum()
 
+    ESD = ESD_calculator(ClusterList, ker, ClusterSum)
+    Old_ESD=ESD+1
     z = 0
     numb_changes = 1
 
-    while numb_changes!=0 and z < 100:
+    while (ESD-Old_ESD)<0 and z < 100:#We can also use number of changes since it is equivalent
         numb_changes=0
-        z = 0
+        Old_ESD=ESD
         for f in range(ker.shape[0]):
             a = ker[f, f]
             Dist = []  # List where we store the "distance" of point f from the Clusters
@@ -121,7 +118,9 @@ def kernel_cluster(ker, ClusterList, NumberOfClusters):
             if Closest_Cluster!=ex_cluster:
                 numb_changes+=1
         z = z + 1
-        print('Number Of Changes:', numb_changes)
+        ESD = ESD_calculator(ClusterList, ker, ClusterSum)
+        # print('ESD Variation', ESD-Old_ESD)
+        # print('Number Of Changes:', numb_changes)
     return ClusterSum
 
 def print_my_table(X,number_of_clusters,cluster_list):
@@ -134,34 +133,45 @@ def print_my_table(X,number_of_clusters,cluster_list):
     print(tabulate(summary, headers=head, tablefmt="github"))
     return
 
+
+
+
+
+pd.set_option('display.max_columns', 500)
+df = pd.read_csv('EastWestAirlinesCluster.csv')
+array = df.to_numpy(dtype=np.float32)
+array2=array.copy()
+# array = normalize_z_score(array)
+# array = normalize_min_max(array)
+array = np.delete(array, 0, 1)
+array2= np.delete(array2, 0, 1)
+
+
+
+
 NumberOfClusters = 4
+NumberOfIterations=5
 gamma= gamma_parameter(array)
-
-
-
-Best_SSE=0
-n=2
+ker = G_kernel_matrix(array, gamma)
+Best_ESD=0
 Best_cluster_list=[]
 t=time.time()
 
-
-for i in range(n):
-    print('Checking:',i+1,'/',n, 'Time:',time.time()-t)
-    SSE=0
+cc=0
+for i in range(NumberOfIterations):
+    print('Checking:',i+1,'/',NumberOfIterations, 'Time:',time.time()-t)
     ClusterList = cluster_start(NumberOfClusters, array)
-    ker = kernel_matrix(array, gamma)
     ClusterSum = kernel_cluster(ker, ClusterList, NumberOfClusters)
+    ESD=ESD_calculator(ClusterList,ker,ClusterSum)
 
-    SSE=meanvar(ClusterList,ker,ClusterSum)
-    if i==1:
-        Best_SSE=SSE+1
+    if i==0:
+        Best_ESD=ESD+1
 
-    if SSE< Best_SSE:
+    if ESD< Best_ESD:
         Best_cluster_list=ClusterList.copy()
-        Best_SSE=SSE
+        Best_ESD=ESD
+        cc +=1
 
-
-
-
+print('Best esd', cc)
 print_my_table(array2,NumberOfClusters,Best_cluster_list)
 
